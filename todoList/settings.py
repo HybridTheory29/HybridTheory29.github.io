d758af5d3c8db32b39730f541707fc3311a6cdaa
HybridTheory29 <kirdyashkin.kiroman@gmail.com>
2025-01-06 12:55:53 +0300

    up

--- todoList/settings.py
+++ todoList/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u7a3)70s*l9fnikk4wncp9jdvy^t@7c7+m+4f9rk7f#bv@$3!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['todolist-1cab.onrender.com', 'localhost', '127.0.0.1']
 
@@ -124,7 +124,9 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
