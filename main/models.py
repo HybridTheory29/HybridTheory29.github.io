588b3a7ad2314852ab1ba7af9bd9c5349949ab6c
HybridTheory29 <kirdyashkin.kiroman@gmail.com>
2025-02-15 18:33:04 +0300

    mobile ver edit

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100, default="")
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     important = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
