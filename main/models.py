ace27cac4bdf970c5a861ebe73146694b33fe760
HybridTheory29 <kirdyashkin.kiroman@gmail.com>
2025-02-21 17:45:08 +0300

    up

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from datetime import datetime
 
 class Category(models.Model):
-    name = models.CharField(max_length=22, default="")
+    name = models.CharField(max_length=50, default="")
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     important = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
